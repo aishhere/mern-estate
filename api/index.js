527757888f8bbcab17cf4f05d752e652fd40e934
aishhere <aishsingh1010@gmail.com>
2025-08-29 18:57:07 +0530

    create sign in api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -14,6 +16,8 @@ mongoose.connect(process.env.MONGO)
 });
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(express.json());
 
